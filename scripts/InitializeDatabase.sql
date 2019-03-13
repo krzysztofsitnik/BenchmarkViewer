129f6813086112b169cf6e6c7534e44af0bd2566
Krzysztof Sitnik <k.sitnik1@gmail.com>
2019-03-14 00:20:09 +0100

    Initial db setup

--- scripts/InitializeDatabase.sql
+++ scripts/InitializeDatabase.sql
@@ -7,5 +7,9 @@ USE BenchmarkViewer
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Benchmarks')
 	CREATE TABLE Benchmarks(
 		BenchmarkID VARCHAR(512) NOT NULL,
+		Date DATETIME NOT NULL,
+		Value DECIMAL NOT NULL,
+		MetricName VARCHAR(32) NOT NULL,
+		Unit VARCHAR(32) NOT NULL,
 		PRIMARY KEY (BenchmarkID))
-		  
+	CREATE INDEX Index1 on Benchmarks (Date, BenchmarkID)
