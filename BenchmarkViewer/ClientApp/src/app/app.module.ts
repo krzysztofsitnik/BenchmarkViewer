65a47d684985f4a788c877754be86596edc87ae7
Krzysztof Sitnik <k.sitnik1@gmail.com>
2019-03-18 15:02:22 +0100

    Added sample charts to see if angular-google-charts component works

--- BenchmarkViewer/ClientApp/src/app/app.module.ts
+++ BenchmarkViewer/ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { GoogleChartsModule } from 'angular-google-charts';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -24,6 +25,7 @@ import { ChartsComponent } from './charts/charts.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    GoogleChartsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
--- BenchmarkViewer/ClientApp/src/app/charts/charts.component.ts
+++ BenchmarkViewer/ClientApp/src/app/charts/charts.component.ts
@@ -7,9 +7,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartsComponent implements OnInit {
 
-  constructor() { }
+  chart1 = {
+      title: 'FirstSample',
+    type: 'LineChart',
+    data: [
+      ['01/01/2019', 12.94, 15],
+      ['02/01/2019', 10.49, 13],
+      ['03/01/2019', 19.30, 13],
+      ['04/01/2019', 21.45, 4],
+    ],
+    columnNames: ['Date', 'Linux', 'Windows'],
+    options: {
+      colors: ['#0000ff', '#ff0000'],
+      animation: {
+        duration: 250,
+        easing: 'ease-in-out',
+        startup: true
+      }
+    }
+  };
 
-  ngOnInit() {
-  }
+  chart2 = {
+    title: 'SecondSample',
+    type: 'LineChart',    
+    data: [
+      ['01/01/2019', 12.94, 25, 10],
+      ['02/01/2019', 10.49, 13, 2],
+      ['03/01/2019', 19.30, 13, 30],
+      ['04/01/2019', 21.45, 4, 12.1],
+    ],
+    columnNames: ['Date', 'Linux', 'Windows', 'X'],
+
+    options: {
+      colors: ['#0000ff', '#ff0000', '#cc66ff'],
+      lineWidth: 3,      
+      legend: { position: 'bottom' },
+      curveType: 'function',
+      displayRangeSelector: true,
+      series: {
+        0: { type: 'line' },
+        1: { type: 'line' },
+        2: { type: 'line', lineWidth: 1 }
+      },
+      animation: {
+        duration: 250,
+        easing: 'ease-in-out',
+        startup: true
+      },
+      hAxis: {
+        title: 'Date',
+      },
+      vAxis: {
+        title: 'Time',
+        },
+        pointSize: 5,
+    }
+  };
+
+    chart3 = {
+        type: 'Table'
+    };
+      ngOnInit() {
+      }
 
 }
