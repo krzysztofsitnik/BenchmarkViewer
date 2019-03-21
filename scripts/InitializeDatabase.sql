e571d6b8357b214103862d600f6bd5812a9e1ef1
Krzysztof Sitnik <k.sitnik1@gmail.com>
2019-03-21 13:19:11 +0100

    Delete tables if they exists, use NVARCHAR instead of VARCHAR

--- scripts/InitializeDatabase.sql
+++ scripts/InitializeDatabase.sql
@@ -3,9 +3,12 @@ IF NOT EXISTS(SELECT * FROM sys.databases WHERE NAME='BenchmarkViewer')
 	CREATE DATABASE BenchmarkViewer
 USE BenchmarkViewer
 
+DROP TABLE BenchmarkMeasurments
+DROP TABLE Benchmarks
+
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Benchmarks')
 	CREATE TABLE Benchmarks(
-		Benchmark VARCHAR(512) NOT NULL UNIQUE,
+		Benchmark NVARCHAR(512) NOT NULL UNIQUE,
 		BenchmarkID INT NOT NULL PRIMARY KEY IDENTITY (1,1))			
 
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='BenchmarkMeasurments')
